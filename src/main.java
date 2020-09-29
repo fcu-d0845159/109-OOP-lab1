fa906c5a0f02d0db3e06ed21f8535b8dffd018e7
user <user@EE01>
2020-09-29 09:54:53 +0800

    123

---
+++ src/main.java
@@ -0,0 +1,9 @@
+
+public class main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm Chiang,Yi-Ping");
+		System.out.println("My nid is D0845159");
+		System.out.println("This is lab 1 hw");
+	}
+}
